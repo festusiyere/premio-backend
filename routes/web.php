2b6fbc29f9bc95ffb4f1a5ec7dcc6d0ba44c6dec
daniel <godwindaniel101@gmail.com>
2021-08-01 00:15:04 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pop-up/{id}' ,[PopupController::class , 'index'] );
+Route::get('/pop-up/{id}' ,[PopupController::class , 'show'] );
