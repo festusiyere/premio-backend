8946a11b9d92200b4ba400ae6d8742003c51f3b5
daniel <godwindaniel101@gmail.com>
2021-08-01 02:04:45 +0100

    first commit

--- resources/views/pop-up.blade.php
+++ resources/views/pop-up.blade.php
@@ -1,28 +1,46 @@
-var main_text_top = "{{$popUpData['mainText']['top']}}"
-var main_text_left = "{{$popUpData['mainText']['left']}}"
-var main_text_content = "{{$popUpData['mainText']['content']}}"
+var main_text_top = "{{ $popUpData['mainText']['top'] }}"
+var main_text_left = "{{ $popUpData['mainText']['left'] }}"
+var main_text_content = "{{ $popUpData['mainText']['content'] }}"
 
-var email_top = "{{$popUpData['email']['top']}}"
-var email_left = "{{$popUpData['email']['left']}}"
-var email_content = "{{$popUpData['email']['content']}}"
+var email_top = "{{ $popUpData['email']['top'] }}"
+var email_left = "{{ $popUpData['email']['left'] }}"
+var email_content = "{{ $popUpData['email']['content'] }}"
 
-var button_top = "{{$popUpData['button']['top']}}"
-var button_left = "{{$popUpData['button']['left']}}"
-var button_content = "{{$popUpData['button']['content']}}"
+var button_top = "{{ $popUpData['button']['top'] }}"
+var button_left = "{{ $popUpData['button']['left'] }}"
+var button_content = "{{ $popUpData['button']['content'] }}"
 
-var supporting_top = "{{$popUpData['supportingText']['top']}}"
-var supporting_text_left = "{{$popUpData['supportingText']['left']}}"
-var supporting_text_content = "{{$popUpData['supportingText']['content']}}"
+var supporting_top = "{{ $popUpData['supportingText']['top'] }}"
+var supporting_text_left = "{{ $popUpData['supportingText']['left'] }}"
+var supporting_text_content = "{{ $popUpData['supportingText']['content'] }}"
 
-var background_color = "{{$popup['background-color']}}"
+var background_color = "{{ $popup['background-color'] }}"
 
 
 
-popBtn = 'position:fixed;right:10px;bottom:20px;width:80px;height:38px;z-index:100;background:green;cursor:pointer;border-radius:10px';
-popModual = 'position : fixed; width:400px;height:0 right:10px;bottom 70px; backgound-color:lightgreen'
+popBtn = `position:fixed;
+          right:10px;
+          bottom:20px;
+          width:80px;
+          height:38px;
+          z-index:10;
+          background:green;
+          cursor:pointer;
+          color:white;
+          border-radius:10px`;
+
+popModual = `transition:all 0.3s ease-in-out;
+             position : fixed;
+             right:10px;
+             bottom:20px;
+             z-index:10; 
+             width:300px;
+             height:0
+             backgound-color:lightgreen`
+
 
 var popBtnElement = document.createElement('div');
-var popModalElement =  document.createElement('div');
+var popModalElement = document.createElement('div');
 
 popBtnElement.classList.add('pop-up-botton');
 popModalElement.classList.add('pop-up-modal');
@@ -31,13 +49,13 @@ popBtnElement.setAttribute('style' , popBtn)
 popModalElement.setAttribute('style' , popModual)
 
 
-popBtnElement.innerHTML  = 'Click me'
+popBtnElement.innerHTML = 'Click me'
 
 
 document.body.appendChild(popBtnElement);
 document.body.appendChild(popModalElement);
 
 popBtnElement.addEventListener('click', function () {
-    popModalElement.style.height = '600px'
+popModalElement.style.height = '400px'
 })
 console.log('yesss');
