7aa0269a766834a0836921151339aa7ac0d1c28d
daniel <godwindaniel101@gmail.com>
2021-08-01 01:05:13 +0100

    first commit

--- resources/views/pop-up.blade.php
+++ resources/views/pop-up.blade.php
@@ -1,20 +1,20 @@
-var main_text_top = {{$popUpData['mainText']['top']}}
-var main_text_left = {{$popUpData['mainText']['left']}}
-var main_text_content = {{$popUpData['mainText']['content']}}
+var main_text_top = "{{$popUpData['mainText']['top']}}"
+var main_text_left = "{{$popUpData['mainText']['left']}}"
+var main_text_content = "{{$popUpData['mainText']['content']}}"
 
-var email_top = {{$popUpData['email']['top']}}
-var email_left = {{$popUpData['email']['left']}}
-var email_content = {{$popUpData['email']['content']}}
+var email_top = "{{$popUpData['email']['top']}}"
+var email_left = "{{$popUpData['email']['left']}}"
+var email_content = "{{$popUpData['email']['content']}}"
 
-var button_top = {{$popUpData['button']['top']}}
-var button_left = {{$popUpData['button']['left']}}
-var button_content = {{$popUpData['button']['content']}}
+var button_top = "{{$popUpData['button']['top']}}"
+var button_left = "{{$popUpData['button']['left']}}"
+var button_content = "{{$popUpData['button']['content']}}"
 
-var supporting_top = {{$popUpData['supportingText']['top']}}
-var supporting_text_left = {{$popUpData['supportingText']['left']}}
-var supporting_text_content = {{$popUpData['supportingText']['content']}}
+var supporting_top = "{{$popUpData['supportingText']['top']}}"
+var supporting_text_left = "{{$popUpData['supportingText']['left']}}"
+var supporting_text_content = "{{$popUpData['supportingText']['content']}}"
 
-var background_color = {{$popup['background-color']}}
+var background_color = "{{$popup['background-color']}}"
 
 
 
